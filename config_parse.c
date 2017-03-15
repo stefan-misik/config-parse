7e0e98e671d3a1e792519ed1f776d24f0610e468
stefan-misik <mail@stefanmisik.eu>
2017-03-15 22:17:42 +0100

    Added initialization and deinitialiaztion code, added Makefile dependency files into gitignore

--- config_parse.c
+++ config_parse.c
@@ -1,2 +1,154 @@
 #include <stdio.h>
 #include "config_parse.h"
+
+
+/******************************************************************************/
+/*                               Private                                      */
+/******************************************************************************/
+
+/* Buffer capacity of a static buffer */
+#define STATIC_BUFFER_CAPACITY ((size_t)-1)
+
+/* Minimal capacity of buffers */
+#define CONFIG_BUFFER_MIN_CAPACITY 128
+
+/**
+ * @brief Initialize config string buffer
+ * 
+ * @param[out] buff Buffer object to be initialized
+ */
+static void config_buffer_init(
+    config_buffer_t * buff
+)
+{
+    buff->capacity = 0;
+    buff->length = 0;
+    buff->data = NULL;
+}
+
+/**
+ * @brief De-initialize config buffer
+ * 
+ * @param[in,out] buff Buffer object to be de-initialized
+ */
+static void config_buffer_deinit(
+    config_buffer_t * buff
+)
+{
+    /* Free buffer */
+    if(NULL != buff->data)
+    {
+        free(buff->data);
+    }
+    
+    /* Zero the fields */
+    buff->capacity = 0;
+    buff->length = 0;
+    buff->data = NULL;
+}
+
+/**
+ * @brief Get the next character of a configuration
+ * 
+ * @param[in,out] cp Config parse object
+ * @return The next character from configuration, or EOF
+ */
+static int config_parse_getc(
+    config_parse_t * cp
+)
+{
+    /* If at the end of the buffer */
+    if(cp->buffer_pos >= cp->buffer.length)
+    {
+        /* If possible read another data from the file */
+        if(NULL != cp->source)
+        {
+            cp->buffer.length = fread(cp->buffer.data, sizeof(char),
+                cp->buffer.capacity, cp->source);
+            cp->buffer_pos = 0;
+            
+            /* If no new data were read, return EOF */
+            if(0 == cp->buffer.length)
+                return EOF;
+        }
+        else
+            return EOF;
+    }
+    
+    /* Get the next character */       
+    return cp->buffer.data[cp->buffer_pos ++];
+}
+
+/**
+ * @brief Initialize config parse object
+ * 
+ * @param[out] cp Config parse object to be initialized
+ */
+static void config_parse_init(
+    config_parse_t * cp
+)
+{
+    /* Initialize buffers */
+    config_buffer_init(&(cp->variable_name));
+    config_buffer_init(&(cp->value));
+    config_buffer_init(&(cp->buffer));
+    
+    /* Initialize other fields */
+    cp->buffer_pos = 0;
+    cp->source = NULL;
+}
+
+/******************************************************************************/
+/*                                Public                                      */
+/******************************************************************************/
+
+/******************************************************************************/
+int config_parse_file_init(
+    config_parse_t * cp,
+    const char * filename
+)
+{
+    /* Do common initialization */
+    config_parse_init(cp);
+
+    return -1;
+}
+
+/******************************************************************************/
+int config_parse_string_init(
+    config_parse_t * cp,
+    const char * config,
+    size_t config_length
+)
+{
+    /* Do common initialization */
+    config_parse_init(cp);
+
+    /* Indicate the configuration the buffer is static */
+    cp->buffer.capacity = STATIC_BUFFER_CAPACITY;
+    /* Load passed string into configuration buffer */
+    cp->buffer.length = config_length;
+    cp->buffer.data = (char *)config;
+
+    return -1;
+}
+
+/******************************************************************************/
+void config_parse_deinit(
+    config_parse_t * cp
+)
+{
+    /* Destroy buffers */
+    config_buffer_deinit(&(cp->variable_name));
+    config_buffer_deinit(&(cp->value));
+
+    if(STATIC_BUFFER_CAPACITY != cp->buffer.capacity)
+        config_buffer_deinit(&(cp->buffer));
+
+    /* Close config file */
+    if(NULL != cp->source)
+    {
+        fclose(cp->source);
+        cp->source = NULL;
+    }
+}
--- config_parse.h
+++ config_parse.h
@@ -1,6 +1,69 @@
 #ifndef CONFIG_PARSE_H
 #define CONFIG_PARSE_H
 
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * @brief Config string buffer
+ * 
+ */
+typedef struct config_buffer
+{
+    char * data;        /** < Pointer to the string buffer containing the */
+    size_t capacity;    /** < Length of the buffer */
+    size_t length;      /** < Length of the string in the buffer */
+} config_buffer_t;
+
+
+/**
+ * @brief Config parse object structure
+ *  
+ */
+typedef struct confg_parse
+{
+    config_buffer_t variable_name;  /** < Name of the current cariable */
+    config_buffer_t value;          /** < Value of the current variable */
+    FILE * source;                  /** < File containing the configuration */
+    config_buffer_t buffer;         /** < Config buffer */
+    size_t buffer_pos;              /** < Position in the buffer */    
+} config_parse_t;
+
+
+/**
+ * @brief Initialize configuration parser and load configuration from a file
+ * 
+ * @param[out] cp       Configuration object to be initialized
+ * @param      filename Configuration file
+ * @return 0 on success
+ */
+int config_parse_file_init(
+    config_parse_t * cp,
+    const char * filename
+);
+
+/**
+ * @brief Initialize configuration parser and load configuration from a string
+ * 
+ * @param[out] cp            Configuration object to be initialized
+ * @param[in]  config        The configuration string
+ * @param      config_length Length of the configuration string
+ * @return 0 on success
+ */
+int config_parse_string_init(
+    config_parse_t * cp,
+    const char * config,
+    size_t config_length
+);
+
+/**
+ * @brief Destroy config parse object
+ * 
+ * @param[in,out] cp Config parse object to be destroyed
+ */
+void config_parse_deinit(
+    config_parse_t * cp
+);
 
 #endif /* CONFIG_PARSE_H */
 
